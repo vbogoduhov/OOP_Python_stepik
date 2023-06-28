e8fc0d3e4b7db7f821ce4226e6e0e07bd8089cae
vbogoduhov <vbogoduhov@gmail.com>
2023-06-28 16:29:13 +0500

    Modified README and add exercises 28-06-2023.

---
+++ 1.4 Методы классов, параметр self/ex_1.3.5.py
@@ -0,0 +1,12 @@
+class Graph:
+    LIMIT_Y = [0, 10]
+
+    def set_data(self, data):
+        self.data = data
+
+    def draw(self):
+        print(*[n for n in self.data if Graph.LIMIT_Y[0] <= n <= Graph.LIMIT_Y[1]])
+
+graph_1 = Graph()
+graph_1.set_data([10, -5, 100, 20, 0, 80, 45, 2, 5, 7])
+graph_1.draw()

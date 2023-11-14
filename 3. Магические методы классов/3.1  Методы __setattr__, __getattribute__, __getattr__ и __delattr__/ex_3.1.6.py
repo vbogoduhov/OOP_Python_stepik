5245bb45e024d3bff9b56b919aca27f782414668
vbogoduhov <vbogoduhov@gmail.com>
2023-11-14 12:43:50 +0500

    Add exercises in 3.1. Modifier exercises.md.

---
+++ 3. Магические методы классов/3.1  Методы __setattr__, __getattribute__, __getattr__ и __delattr__/ex_3.1.6.py
@@ -0,0 +1,40 @@
+class Museum:
+    def __init__(self, name):
+        self.name = name
+        self.exhibits = []
+
+    def add_exhibit(self, obj):
+        self.exhibits.append(obj)
+
+    def remove_exhibit(self, obj):
+        self.exhibits.remove(obj)
+
+    def get_info_exhibit(self, indx):
+        return f"Описание экспоната {self.exhibits[indx].name}: {self.exhibits[indx].descr}"
+
+class Picture:
+    def __init__(self, name, author, descr):
+        self.name = name
+        self.author = author
+        self.descr = descr
+
+
+class Mummies:
+    def __init__(self, name, author, descr):
+        self.name = name
+        self.author = author
+        self.descr = descr
+
+class Papyri:
+    def __init__(self, name, author, descr):
+        self.name = name
+        self.author = author
+        self.descr = descr
+
+mus = Museum("Эрмитаж")
+mus.add_exhibit(Picture("Балакирев с подписчиками пишет письмо иноземному султану", "Неизвестный автор", "Вдохновляющая, устрашающая, волнующая картина"))
+mus.add_exhibit(Mummies("Балакирев", "Древняя Россия", "Просветитель XXI века, удостоенный мумификации"))
+p = Papyri("Ученья для, не злата ради", "Древняя Россия", "Самое древнее найденное рукописное свидетельство о языках программирования")
+mus.add_exhibit(p)
+for i in range(len(mus.exhibits) + 1):
+    print(mus.get_info_exhibit(i))

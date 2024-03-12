0955649b3e76a3ee296c94d2a654a03b01e1de3a
vbogoduhov <vbogoduhov@gmail.com>
2024-03-12 16:53:20 +0500

    Start ex_4.6.5.

---
+++ 4. Наследование и полиморфизм/4.6 Множественное наследование/ex_4.6.5.py
@@ -0,0 +1,31 @@
+class ShopItem:
+    ID_SHOP_ITEM = 0
+
+    def __init__(self):
+        super().__init__()
+        ShopItem.ID_SHOP_ITEM += 1
+        self._id = ShopItem.ID_SHOP_ITEM
+
+    def get_pk(self):
+        return self._id
+
+
+# здесь объявляйте классы ShopGenericView и ShopUserView
+
+
+class Book(ShopItem):
+    def __init__(self, title, author, year):
+        super().__init__()
+        self._title = title
+        self._author = author
+        self._year = year
+
+
+class ShopGenericView(object):
+    """docstring for ShopGenericView."""
+
+    def __init__(self):
+        super(ShopGenericView, self).__init__()
+
+    def __repr__(self):
+        return f""

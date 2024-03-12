3b545da8a4092dd42bdf6650528731cb6c0e2b05
vbogoduhov <vbogoduhov@gmail.com>
2024-03-12 17:03:28 +0500

    Add exercises in 4.6. Need edit exercises.md.

--- 4. Наследование и полиморфизм/4.6 Множественное наследование/ex_4.6.5.py
+++ 4. Наследование и полиморфизм/4.6 Множественное наследование/ex_4.6.5.py
@@ -13,14 +13,6 @@ class ShopItem:
 # здесь объявляйте классы ShopGenericView и ShopUserView
 
 
-class Book(ShopItem):
-    def __init__(self, title, author, year):
-        super().__init__()
-        self._title = title
-        self._author = author
-        self._year = year
-
-
 class ShopGenericView(object):
     """docstring for ShopGenericView."""
 
@@ -28,4 +20,32 @@ class ShopGenericView(object):
         super(ShopGenericView, self).__init__()
 
     def __repr__(self):
-        return f""
+        return f"\n".join([f"{item[0]}: {item[1]}" for item in self.__dict__.items()])
+
+
+class ShopUserView(object):
+    """docstring for ShopUserView."""
+
+    def __init__(self):
+        super(ShopUserView, self).__init__()
+
+    def __str__(self):
+        return f"\n".join(
+            [
+                f"{item[0]}: {item[1]}"
+                for item in self.__dict__.items()
+                if item[0] != "_id"
+            ]
+        )
+
+
+class Book(ShopItem, ShopUserView):
+    def __init__(self, title, author, year):
+        super().__init__()
+        self._title = title
+        self._author = author
+        self._year = year
+
+
+book = Book("Python ООП", "Балакирев", 2022)
+print(book)

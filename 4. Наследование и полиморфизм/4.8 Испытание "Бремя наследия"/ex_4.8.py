ed29e4354f8add51366da4f15ada2543d1017a72
Vladimir <vladimir@localhost.localdomain>
2024-03-18 17:09:46 +0500

    Start 4.8 and ex_4.8.

---
+++ 4. Наследование и полиморфизм/4.8 Испытание "Бремя наследия"/ex_4.8.py
@@ -0,0 +1,175 @@
+class Vertex(object):
+    """docstring for Vertex."""
+
+    def __init__(self):
+        super(Vertex, self).__init__()
+        self._links = []
+
+    @property
+    def links(self):
+        """The links property."""
+        return self._links
+
+
+class Link(object):
+    """docstring for Link."""
+
+    def __init__(self, v1, v2, dist=1):
+        super(Link, self).__init__()
+        self._v1 = v1
+        self._v2 = v2
+        if self._v2 not in self._v1._links:
+            self._v1._links.append(self._v2)
+        if self._v1 not in self._v2._links:
+            self._v2._links.append(self._v1)
+        self._dist = dist
+
+    @property
+    def v1(self):
+        """The v1 property."""
+        return self._v1
+
+    @property
+    def v2(self):
+        """The v2 property."""
+        return self._v2
+
+    @property
+    def dist(self):
+        """The dist property."""
+        return self._dist
+
+    @dist.setter
+    def dist(self, value):
+        self._dist = value
+
+    def __eq__(self, other):
+        return self.v1 in (other.v1, other.v2) and self.v2 in (other.v1, other.v2)
+
+
+class LinkedGraph(object):
+    """docstring for LinkedGraph."""
+
+    def __init__(self):
+        super(LinkedGraph, self).__init__()
+        self._links = []
+        self._vertex = []
+
+    def add_vertex(self, v):
+        if v not in self._vertex:
+            self._vertex.append(v)
+
+    def add_link(self, link):
+        if link not in self._links:
+            self._links.append(link)
+            self.add_vertex(link.v1)
+            self.add_vertex(link.v2)
+
+    def find_path(self, start_v, stop_v):
+        pass
+
+
+class Station(Vertex):
+    """docstring for Station."""
+
+    def __init__(self, name):
+        super(Station, self).__init__()
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+class LinkMetro(Link):
+    """docstring for LinkMetro."""
+
+    def __init__(self, v1, v2, dist):
+        super(LinkMetro, self).__init__(v1, v2, dist)
+
+
+# map2 = LinkedGraph()
+# v1 = Vertex()
+# v2 = Vertex()
+# v3 = Vertex()
+# v4 = Vertex()
+# v5 = Vertex()
+#
+# map2.add_link(Link(v1, v2))
+# map2.add_link(Link(v2, v3))
+# map2.add_link(Link(v2, v4))
+# map2.add_link(Link(v3, v4))
+# map2.add_link(Link(v4, v5))
+#
+# assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
+# assert (
+#     len(map2._vertex) == 5
+# ), "неверное число вершин в списке _vertex класса LinkedGraph"
+#
+# map2.add_link(Link(v2, v1))
+# assert (
+#     len(map2._links) == 5
+# ), "метод add_link() добавил связь Link(v2, v1), хотя уже имеется связь Link(v1, v2)"
+#
+# path = map2.find_path(v1, v5)
+# s = sum([x.dist for x in path[1]])
+# assert (
+#     s == 3
+# ), "неверная суммарная длина маршрута, возможно, некорректно работает объект-свойство dist"
+#
+# assert (
+#     issubclass(Station, Vertex) and issubclass(LinkMetro, Link)
+# ), "класс Station должен наследоваться от класса Vertex, а класс LinkMetro от класса Link"
+#
+# map2 = LinkedGraph()
+# v1 = Station("1")
+# v2 = Station("2")
+# v3 = Station("3")
+# v4 = Station("4")
+# v5 = Station("5")
+#
+# map2.add_link(LinkMetro(v1, v2, 1))
+# map2.add_link(LinkMetro(v2, v3, 2))
+# map2.add_link(LinkMetro(v2, v4, 7))
+# map2.add_link(LinkMetro(v3, v4, 3))
+# map2.add_link(LinkMetro(v4, v5, 1))
+#
+# assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
+# assert (
+#     len(map2._vertex) == 5
+# ), "неверное число вершин в списке _vertex класса LinkedGraph"
+#
+# path = map2.find_path(v1, v5)
+#
+# assert str(path[0]) == "[1, 2, 3, 4, 5]", path[0]
+# s = sum([x.dist for x in path[1]])
+# assert s == 7, "неверная суммарная длина маршрута для карты метро"
+map_metro = LinkedGraph()
+v1 = Station("1")
+v2 = Station("2")
+v3 = Station("3")
+v4 = Station("4")
+v5 = Station("5")
+v6 = Station("6")
+
+map_metro.add_link(LinkMetro(v1, v2, 1))
+map_metro.add_link(LinkMetro(v1, v3, 1))
+map_metro.add_link(LinkMetro(v1, v4, 4))
+map_metro.add_link(LinkMetro(v3, v4, 2))
+map_metro.add_link(LinkMetro(v2, v5, 5))
+map_metro.add_link(LinkMetro(v4, v5, 2))
+map_metro.add_link(LinkMetro(v4, v6, 1))
+map_metro.add_link(LinkMetro(v5, v6, 4))
+
+
+def create_matrix(lg):
+    pass
+
+
+print(len(map_metro._vertex))
+for vertex in map_metro._vertex:
+    print(f"vertex -- {vertex.name}")
+    for v in vertex._links:
+        print(f"link to {v.name}")

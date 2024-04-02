011715b22d0b0d1b13865b3e8c95dcec991fe693
vbogoduhov <vbogoduhov@gmail.com>
2024-04-02 17:13:26 +0500

    Modified sea_battle. Need to test. Wrong function <is_collide>.

--- 5. Исключения и менеджеры контекста/5.6 Посвящение в объектно-ориентированное программирование/sea_battle.py
+++ 5. Исключения и менеджеры контекста/5.6 Посвящение в объектно-ориентированное программирование/sea_battle.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Ship(object):
     """Класс, описывающий корабль"""
 
@@ -51,7 +54,7 @@ class Ship(object):
                 ):
                     return True
         elif (
-            self._tp == 1 and ship_tp == 2
+            self._tp == 1 and ship._tp == 2
         ):  # Ориентация кораблей не одинакова, текущий - горизонтальный, второй - вертикальный
             if not (
                 (ship_coord[1] - self._y) > 1
@@ -60,7 +63,7 @@ class Ship(object):
                 or (self._x - ship_coord[0]) > 1
             ):
                 return True
-        else:  # Ориентация не одинакова, текущий - вертикальный, воторой - горизонтальный
+        else:  # Ориентация не одинакова, текущий - вертикальный, второй - горизонтальный
             if not (
                 (ship_coord[1] - (self._y + self._length - 1)) > 1
                 or (self._y - ship_coord[1]) > 1
@@ -90,6 +93,22 @@ class Ship(object):
             raise ValueError("Неверное значение для коллекции _cells")
         self._cells[key] = value
 
+    def __eq__(self, ship):
+        coord = ship.get_start_coords()
+        if (
+            self._length == ship._length
+            and self._tp == ship._tp
+            and self._x == coord[0]
+            and self._y == coord[1]
+        ):
+            return True
+        return False
+
+    def get_incremental_coord(self, step):
+        if self._tp == 1:
+            return self._x + step, self._y
+        return self._x, self._y + step
+
 
 class GamePole(object):
     """Класс, описывающий игровое поле"""
@@ -101,21 +120,99 @@ class GamePole(object):
 
     def init(self):
         """Инициализация игрового поля"""
-        pass
+        self._ships = [
+            Ship(4, tp=randint(1, 2)),
+            Ship(3, tp=randint(1, 2)),
+            Ship(3, tp=randint(1, 2)),
+            Ship(2, tp=randint(1, 2)),
+            Ship(2, tp=randint(1, 2)),
+            Ship(2, tp=randint(1, 2)),
+            Ship(1, tp=randint(1, 2)),
+            Ship(1, tp=randint(1, 2)),
+            Ship(1, tp=randint(1, 2)),
+            Ship(1, tp=randint(1, 2)),
+        ]
+        # Теперь нужно расставить корабли
+        self.__shuffle_ships()
+
+    def __shuffle_ships(self):
+        count = 0
+        while count < self._size:
+            ship = self._ships[count]
+            while True:
+                ship.set_start_coords(randint(0, 9), randint(0, 9))
+                if self.is_not_collide_all(
+                    ship, self._ships[:count]
+                ) and not ship.is_out_pole(self._size):
+                    break
+            count += 1
+
+    def is_not_collide_all(self, ship, lst_ships=None):
+        if lst_ships is None:
+            lst = self._ships
+        else:
+            lst = lst_ships
+        for other in lst:
+            if ship != other:
+                if ship.is_collide(other):
+                    return False
+        return True
 
     def get_ships(self):
         """Для получения коллекции _ships"""
         return self._ships
 
     def move_ships(self):
-        """Перемещает все корабли на одну клетку по напраалению оринтации"""
-        pass
+        """Перемещает все корабли на одну клетку по направлению ориентации"""
+        for ship in self._ships:
+            coord = ship.get_start_coords()
+            # продумать как автоматически увеличивать нужнуюю координату на нужное количество клеток, в зависимости от ориентации
+            new_coord = ship.get_incremental_coord(1)
+            ship.set_start_coords(new_coord[0], new_coord[1])
+            if self.is_not_collide_all(ship) and not ship.is_out_pole(self._size):
+                ship.set_start_coords(coord[0], coord[1])
+                ship.move(1)
+            else:
+                new_coord = ship.get_incremental_coord(-2)
+                ship.set_start_coords(new_coord[0], new_coord[1])
+                if self.is_not_collide_all(ship) and not ship.is_out_pole(self._size):
+                    ship.set_start_coords(coord[0], coord[1])
+                    ship.move(-1)
+                else:
+                    ship.set_start_coords(coord[0], coord[1])
 
     def show(self):
         """Отображение игрового поля в консоли.
         Каждый корабль должен отображаться значениями из коллекции _cells"""
-        pass
+        pole = self.get_pole()
+
+        for row in pole:
+            print(*row)
 
     def get_pole(self):
         """Получение текущего игрового поля в виде двумерного кортежа"""
-        pass
+        pole = [[0 for _ in range(self._size)] for _ in range(self._size)]
+        for ship in self._ships:
+            coord = ship.get_start_coords()
+            length = ship._length
+            tp = ship._tp
+            cells = ship._cells
+            if tp == 1:
+                for x, v in enumerate(cells):
+                    pole[coord[1]][coord[0] + x] = v
+            else:
+                for x, v in enumerate(cells):
+                    pole[coord[1] + x][coord[0]] = v
+        out = tuple([tuple([i for i in row]) for row in pole])
+        return out
+
+
+SIZE_GAME_POLE = 10
+
+pole = GamePole(SIZE_GAME_POLE)
+pole.init()
+pole.show()
+
+pole.move_ships()
+print()
+pole.show()
